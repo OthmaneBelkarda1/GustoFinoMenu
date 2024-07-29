8285734acc26b647068f9e6cbe1a464152ed2ce2
othmanebelkarda <belkardaothmane05@gmail.com>
2024-07-29 22:04:20 +0100

    Update checkout.js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -18,8 +18,8 @@ function success(position) {
 function error() {
     setInterval(()=>{alert("Unable to retrieve your location.")},10)
 }
-var restaurantLatitude = 34.007547055685954; // Example coordinates
-var restaurantLongitude = -6.844985820610778 ; // Example coordinates
+var restaurantLatitude = 33.9170091529695 ; // Example coordinates
+var restaurantLongitude = -6.911961735811822; // Example coordinates
 var geofenceRadius = 100; // Geofence radius in meters
 function checkGeofence(userLatitude, userLongitude) {
     var distance = calculateDistance(userLatitude, userLongitude, restaurantLatitude, restaurantLongitude);
