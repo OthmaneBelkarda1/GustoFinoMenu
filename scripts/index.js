542148918f01429b87854fc6965274aee60f03cc
othmanebelkarda <belkardaothmane05@gmail.com>
2024-07-30 20:42:08 +0100

    /

---
+++ scripts/index.js
@@ -0,0 +1,4 @@
+document.body.style.backgroundImage = "url('/images/BackGround.jpg')";
+document.body.style.backgroundSize = "cover";
+document.body.style.backgroundRepeat = "no-repeat";
+document.body.style.backgroundPosition = "center center";
